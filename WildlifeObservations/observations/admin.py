d92b09646a673877ff1910b9ecd1f6da3945f09d
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-01-21 20:13:37 +0100

    Correct admin

--- WildlifeObservations/observations/admin.py
+++ WildlifeObservations/observations/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Site
-
-admin.site.register(Site)
+from . import models
 
 # Register your models here.
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('area', 'site_name', 'altitude_band',)
+    ordering = ('site_name', 'altitude_band',)
+    search_fields = ('area', 'site_name', 'altitude_band',)
+
+admin.site.register(models.Site, SiteAdmin)
+
