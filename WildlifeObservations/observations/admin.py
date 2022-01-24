b1b38700dba2380da9a28a06dfaaf2362b37076d
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-01-24 19:45:04 +0100

    Do PEP8 formatting

--- WildlifeObservations/observations/admin.py
+++ WildlifeObservations/observations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
 
 class SiteAdmin(admin.ModelAdmin):
@@ -8,5 +9,5 @@ class SiteAdmin(admin.ModelAdmin):
     ordering = ('site_name', 'altitude_band',)
     search_fields = ('area', 'site_name', 'altitude_band',)
 
-admin.site.register(models.Site, SiteAdmin)
 
+admin.site.register(models.Site, SiteAdmin)
