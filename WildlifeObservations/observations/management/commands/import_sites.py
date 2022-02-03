0c007f191d64d6b7aeaca2ce9041650e1d2911d1
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-02-03 18:59:32 +0100

    Add importer for site data

---
+++ WildlifeObservations/observations/management/commands/import_sites.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+
+from ...models import Site
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Adds sites'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    def handle(self, *args, **options):
+        print(options['filename'])
+        self.import_data_from_csv(options['filename'])
+
+    def import_data_from_csv(self, filename):
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            #country_uid, created = Site.objects.get_or_create(uid=None, source=source)
+
+            for row in reader:
+                site = Site()
+                site.area = row['area']
+                site.site_name = row['sitename']
+                site.altitude_band = row['altitude_band']
+                site.transect_length = row['transect_length']
+                site.transect_description = row['transect_description']
+                site.notes = row['notes']
+
+                site.gps_latitude_start = row['start_latitude']
+                site.gps_longitude_start = row['start_longitude']
+                site.gps_altitude_start = row['start_altitude']
+                site.gps_number_satellites_start = row['start_number_satellites']
+                site.gps_accuracy_start = row['start_gps_accuracy']
+                site.gps_aspect_start = row['start_orientation']
+
+                site.gps_latitude_end = row['end_latitude']
+                site.gps_longitude_end = row['end_longitude']
+                site.gps_altitude_end = row['end_altitude']
+                site.gps_number_satellites_end = row['end_number_satellites']
+                site.gps_accuracy_end = row['end_gps_accuracy']
+                site.gps_aspect_end = row['end_orientation']
+
+                site.save()
