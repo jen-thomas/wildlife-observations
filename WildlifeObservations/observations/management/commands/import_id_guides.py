a9bc3a4313f1e48bfb28cdbda763274e7e402086
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-02-11 17:20:13 +0100

    Add importer for ID guides

---
+++ WildlifeObservations/observations/management/commands/import_id_guides.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from ...models import IdentificationGuide
+
+class Command(BaseCommand):
+    help = 'Adds ID guides'
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        self.import_id_guides()
+
+    def import_id_guides(self):
+        id_guide = IdentificationGuide.objects.create(title='Grasshoppers of Britain and Western Europe',
+                                                  author='Sardet, Roesti and Braud')
+        id_guide = IdentificationGuide.objects.create(title='The Orthoptera fauna of the Pyrenean region - a field guide',
+                                                      author='Poniatowski, Defaut, Llucià-Pomares and Fartmann')
+        id_guide = IdentificationGuide.objects.create(title='Atles dels Ortòpters de Catalunya',
+                                                      author='Josep Maria Olmo Vidal')
