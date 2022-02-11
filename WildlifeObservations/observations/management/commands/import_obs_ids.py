8c47f476e790cca14c35414329fc208b7ba8688d
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-02-11 12:31:02 +0100

    First version of importer for observations and identifications

---
+++ WildlifeObservations/observations/management/commands/import_obs_ids.py
@@ -0,0 +1,61 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from datetime import datetime
+
+
+from ...models import Survey, Observation, IdentificationGuide, Identification, Visit, Site
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Adds observations and identifications'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        print(options['filename'])
+        self.import_visit_from_csv(options['filename'])
+
+    def import_observation_from_csv(self, filename):
+
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                observation = Observation()
+
+                survey_details = row['specimen_id'].split
+                site = survey_details[0]
+                visit_date = survey_details[1]
+
+                survey_method = survey_details[2][0]
+                if survey_method == 'N':
+                    method = 'Net'
+                elif survey_method =='H':
+                    method = 'Hand'
+
+                survey_repeat = survey_details[2][1]
+
+                visit_date_time_obj = datetime.strptime(visit_date, '%y%m%d')
+
+                visit = Visit.objects.get(Site.objects.get(site_name=site), date=visit_date_time_obj)
+                survey = Survey.objects.get(visit=visit, method=method, repeat=survey_repeat)
+
+
+                observation.specimen_label = row['specimen_id']
+                observation.survey = survey
+                observation.length_head_abdomen = row['length_mm']
+                observation.status = 'Specimen' # all those imported are specimens rather than observations
+
+                observation.save()
+
+
+def select_columns(row, list_of_columns) -> dict:
+    selected = {}
+
+    for column_name in list_of_columns:
+        selected[column_name] = row[column_name]
+
+    return selected
