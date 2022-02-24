102dc036c83c1fb7d7883784f27ee1dfb66da87f
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-02-24 10:33:12 +0100

    Add reports for observations, observation summary counts and number of identifications.

--- WildlifeObservations/observations/management/commands/report.py
+++ WildlifeObservations/observations/management/commands/report.py
@@ -12,5 +12,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         species_reports = SpeciesReport()
 
+        print("---------- Number of observations ----------")
+        print("Total:", species_reports.observations_count())
+
+        for row in species_reports.observations_suborder_count():
+            print(row["suborder"], row["count"])
+
+        print("\n---------- Number of individual observations identified ----------")
+        print(species_reports.identified_observations_count())
+
+        print("\n---------- Number of each species identified ----------")
         for row in species_reports.species_identified_count():
             print(row["species_name"], row["count"])
+
--- WildlifeObservations/observations/reports.py
+++ WildlifeObservations/observations/reports.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 
-from .models import Identification
+from .models import Identification, Observation
 
 
 class SpeciesReport:
@@ -22,5 +22,35 @@ class SpeciesReport:
 
         for identification in qs:
             result.append({"species_name": identification["species__latin_name"], "count": identification["total"]})
+
+        return result
+
+    def identified_observations_count(self):
+        """Return total number (integer) of individual observations identified."""
+
+        qs = Identification.objects.filter(species__isnull=False).values("observation__specimen_label").count()
+
+        return qs
+
+    def observations_count(self):
+        """Return total number (integer) of individual observations made."""
+
+        qs = Observation.objects.all().count()
+
+        return qs
+
+    def observations_suborder_count(self):
+        """Return list of dictionaries of total numbers of observations of each suborder that have been made.
+
+        For example:
+        [{'suborder': 'Caelifera', 'count':30},
+        {'suborder': 'Ensifera', 'count':60}]"""
+
+        qs = Identification.objects.values("suborder__suborder").annotate(total=Count("suborder"))
+
+        result = []
+
+        for identification in qs:
+            result.append({"suborder": identification["suborder__suborder"], "count": identification["total"]})
 
         return result
