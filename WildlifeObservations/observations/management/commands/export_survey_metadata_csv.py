9d1a808bfdae3c1f4e9a48877ecc060b1ed6070b
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-04 20:33:41 +0200

    Use argparse.FileType instead of strings. Helps error management and reduces code.

--- WildlifeObservations/observations/management/commands/export_survey_metadata_csv.py
+++ WildlifeObservations/observations/management/commands/export_survey_metadata_csv.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 
 from django.core.management.base import BaseCommand
@@ -8,7 +9,7 @@ header_survey = ['site_name', 'date_cest', 'start_time_cest', 'end_time_cest', '
                  'cloud_coverage_start', 'wind_start', 'rain_start', 'cloud_coverage_end', 'wind_end', 'rain_end']
 
 
-def export_csv(file_path):
+def export_csv(output_file):
     """
     Export data from a query into a CSV file which has a specified path and filename.
 
@@ -16,50 +17,41 @@ def export_csv(file_path):
     of headers.
     """
 
-    with open(file_path, 'w') as file:
-        headers = header_survey
+    headers = header_survey
 
-        csv_writer = csv.DictWriter(file, headers)
+    csv_writer = csv.DictWriter(output_file, headers)
 
-        csv_writer.writeheader()
+    csv_writer.writeheader()
 
-        surveys = Survey.objects.all().order_by('visit__site__site_name', 'visit__date', 'start_time')
+    surveys = Survey.objects.all().order_by('visit__site__site_name', 'visit__date', 'start_time')
 
-        for survey in surveys:
+    for survey in surveys:
 
-            survey_site_name = survey.visit.site.site_name
-            survey_date = survey.visit.date
+        survey_site_name = survey.visit.site.site_name
+        survey_date = survey.visit.date
 
-            row = {}
+        row = {}
 
-            row['site_name'] = survey_site_name
-            row['date_cest'] = survey_date
-            row['start_time_cest'] = survey.start_time
-            row['end_time_cest'] = survey.end_time
-            row['method'] = survey.method
-            row['repeat'] = survey.repeat
-            row['cloud_coverage_start'] = survey.meteorologyconditions.cloud_coverage_start
-            row['wind_start'] = survey.meteorologyconditions.wind_start
-            row['rain_start'] = survey.meteorologyconditions.rain_start
-            row['cloud_coverage_end'] = survey.meteorologyconditions.cloud_coverage_end
-            row['wind_end'] = survey.meteorologyconditions.wind_end
-            row['rain_end'] = survey.meteorologyconditions.rain_end
+        row['site_name'] = survey_site_name
+        row['date_cest'] = survey_date
+        row['start_time_cest'] = survey.start_time
+        row['end_time_cest'] = survey.end_time
+        row['method'] = survey.method
+        row['repeat'] = survey.repeat
+        row['cloud_coverage_start'] = survey.meteorologyconditions.cloud_coverage_start
+        row['wind_start'] = survey.meteorologyconditions.wind_start
+        row['rain_start'] = survey.meteorologyconditions.rain_start
+        row['cloud_coverage_end'] = survey.meteorologyconditions.cloud_coverage_end
+        row['wind_end'] = survey.meteorologyconditions.wind_end
+        row['rain_end'] = survey.meteorologyconditions.rain_end
 
-            csv_writer.writerow(row)
+        csv_writer.writerow(row)
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('output_directory', type=str, help='Path to the file or - for stdout')
-        parser.add_argument('file_basename', type=str, help='File basename')
+        parser.add_argument('output_file', type=argparse.FileType('w'), help='Path to the file or - for stdout')
 
     def handle(self, *args, **options):
-        path = options['output_directory']
-        file_name = options['file_basename']
-        file_path = f'{path}/{file_name}.csv'
-
-        if file_path == '-':
-            file_path = '/dev/stdout'
-
-        export_csv(file_path)
+        export_csv(options['output_file'])
