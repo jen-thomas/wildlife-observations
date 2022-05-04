8ce54882da021ba7957c5003e3c5d81c6c06a6ab
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-04 15:10:06 +0200

    Export vegetation surveys.

--- WildlifeObservations/observations/management/commands/export_vegetation_surveys_csv.py
+++ WildlifeObservations/observations/management/commands/export_vegetation_surveys_csv.py
@@ -4,7 +4,9 @@ from django.core.management.base import BaseCommand
 
 from ...models import VegetationStructure, Plot
 
-header_survey = []
+header_vegetation_survey = ['site_name', 'date_cest', 'plot_distance_from_start_m', 'percentage_vegetation_cover',
+                            'percentage_bare_ground', 'percentage_rock', 'height_75percent', 'max_height',
+                            'density_01', 'density_02', 'density_03', 'density_04', 'density_05']
 
 
 def export_csv(file_path):
@@ -16,20 +18,31 @@ def export_csv(file_path):
     """
 
     with open(file_path, 'w') as file:
-        headers = header_survey
+        headers = header_vegetation_survey
 
         csv_writer = csv.DictWriter(file, headers)
-
         csv_writer.writeheader()
 
-        vegetation_surveys = VegetationStructure.objects.all().order_by('')
+        vegetation_surveys = VegetationStructure.objects.all()
 
-        for survey in vegetation_surveys:
+        for vegetation_survey in vegetation_surveys:
+
+            plot = Plot.objects.get(plot=Plot)
             row = {}
 
-            row['site_name'] = survey.visit.site.site_name
-            row['date_cest'] = survey.visit.date
-
+            row['site_name'] = plot.visit.site.site_name
+            row['date_cest'] = plot.visit.date
+            row['plot_distance_from_start_m'] = plot.position
+            row['percentage_vegetation_cover'] = vegetation_survey.percentage_vegetation_cover
+            row['percentage_bare_ground'] = vegetation_survey.percentage_bare_ground
+            row['percentage_rock'] = vegetation_survey.percentage_rock
+            row['height_75percent'] = vegetation_survey.height_75percent
+            row['max_height'] = vegetation_survey.max_height
+            row['density_01'] = vegetation_survey.density_01
+            row['density_02'] = vegetation_survey.density_02
+            row['density_03'] = vegetation_survey.density_03
+            row['density_04'] = vegetation_survey.density_04
+            row['density_05'] = vegetation_survey.density_05
 
             csv_writer.writerow(row)
 
