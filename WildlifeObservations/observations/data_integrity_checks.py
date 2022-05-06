dd792e4d849eed3541267fdf5b23c6958338043e
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-06 15:47:47 +0200

    Create command and code to check the integrity of the data in the database, before it is exported. This will avoid having extra code to account for different situations in the exports.
    Add first check to list all identifications that do not have a sex.

---
+++ WildlifeObservations/observations/data_integrity_checks.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+
+from .models import Identification, Observation
+
+
+class IdentificationDataChecks:
+    def __init__(self):
+        pass
+
+    def check_identification_has_sex(self):
+        """
+        Returns list of dictionaries of the identifications that do not have a sex.
+
+        Note that whilst it might not be possible to determine the sex of an observation if it is not an adult,
+        these identifications should still have sex=UNKNOWN, therefore this query considers all identifications,
+        not just the adults.
+
+        e.g.    [{"specimen_label": TOR08 20211005 H1 C001},
+                {"specimen_label": TAV09 20211006 N1 C008}]
+        """
+
+        identifications = Identification.objects.filter(sex__isnull=True)
+
+        identifications_missing_sex = []
+
+        for identification in identifications:
+            identifications_missing_sex.append({"specimen_label": identification.observation.specimen_label})
+
+        return identifications_missing_sex
---
+++ WildlifeObservations/observations/management/commands/data_checks_identifications.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from ...data_integrity_checks import IdentificationDataChecks
+
+
+class Command(BaseCommand):
+    help = 'Check identification data.'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        identification_checks = IdentificationDataChecks()
+
+        print("***** Identifications without a sex (all stages) *****")
+        print(len(identification_checks.check_identification_has_sex()), "results:\n")
+        for identification in identification_checks.check_identification_has_sex():
+            print(identification['specimen_label'])
