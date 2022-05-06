a444e8325e34eca9b262cbb9ab6ad8e989a82321
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-06 10:42:51 +0200

    Update confidence reasons depending on confidence, if the reason is null (to avoid overwriting those that have already been set).

--- WildlifeObservations/observations/management/commands/update_identification_confidence_reasons.py
+++ WildlifeObservations/observations/management/commands/update_identification_confidence_reasons.py
@@ -1,14 +1,68 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.db.models import Q
 
 from ...models import Identification
 
+
+def confidence_yes_to_confirmed():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.YES).update(
+        confidence=Identification.Confidence.CONFIRMED)
+
+
+def confirmed_add_reason():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.CONFIRMED).update(
+        confidence_reason=Identification.ConfidenceReason.ID_CERTAIN)
+
+
+def check_add_reason():
+    get_identifications_with_confidence_without_reasons(
+        Q(confidence=Identification.Confidence.CHECK) | Q(confidence=Identification.Confidence.CHECK_IN_MUSEUM)).update(
+        confidence_reason=Identification.ConfidenceReason.ID_NEEDS_CONFIRMATION)
+
+
+def review_add_reason():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.REVIEW).update(
+        confidence_reason=Identification.ConfidenceReason.ID_UNCERTAIN)
+
+
+def redo_add_reason():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.REDO).update(
+        confidence_reason=Identification.ConfidenceReason.ID_INCORRECT)
+
+
+def inprogress_add_reason():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.IN_PROGRESS).update(
+        confidence_reason=Identification.ConfidenceReason.ID_INCOMPLETE)
+
+
+def confidence_nymph_to_reason():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.SMALL_NYMPH_HARD_TO_ID).update(
+        confidence=Identification.Confidence.CONFIRMED,
+        confidence_reason=Identification.ConfidenceReason.SMALL_NYMPH_HARD_TO_ID)
+
+
+def confidence_cannot_det_to_reason():
+    get_identifications_with_confidence_without_reasons(Identification.Confidence.CANNOT_DETERMINE_FURTHER).update(
+        confidence=Identification.Confidence.CONFIRMED,
+        confidence_reason=Identification.Confidence.CANNOT_DETERMINE_FURTHER)
+
+
+def get_identifications_with_confidence_without_reasons(confidence):
+    return Identification.objects.filter(confidence=confidence).filter(confidence_reason__isnull=True)
+
+
 class Command(BaseCommand):
-    help = 'Updates confidence reason field in database.'
+    help = 'Updates confidence and confidence reason fields in database.'
 
     @transaction.atomic
     def handle(self, *args, **options):
-        self.confidence_yes_to_confirmed()
 
-    def confidence_yes_to_confirmed(self):
-        identifications = Identification.objects.filter(confidence=Identification.Confidence.YES).update(confidence=Identification.Confidence.CONFIRMED)
+        confidence_yes_to_confirmed()
+        confirmed_add_reason()
+        check_add_reason()
+        review_add_reason()
+        redo_add_reason()
+        inprogress_add_reason()
+        confidence_nymph_to_reason()
+        confidence_cannot_det_to_reason()
